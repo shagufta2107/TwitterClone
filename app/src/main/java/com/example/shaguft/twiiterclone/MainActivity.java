d08b41b080afc8418de1340e827dc7df2d9bcb42
shagufta2107 <shagufta.zhcet09@gmail.com>
2019-01-05 23:06:52 +0530

    Removed app name from toolbar

--- app/src/main/java/com/example/shaguft/twiiterclone/MainActivity.java
+++ app/src/main/java/com/example/shaguft/twiiterclone/MainActivity.java
@@ -13,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar= findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setTitle("");
+        toolbar.setSubtitle("");
     }
 }
