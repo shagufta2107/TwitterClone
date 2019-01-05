b60fd48d7924f233e8665b3874037293fb7fc806
shagufta2107 <shagufta.zhcet09@gmail.com>
2019-01-05 22:43:09 +0530

    Basic Toolbar added

--- app/src/main/java/com/example/shaguft/twiiterclone/Home.java
+++ app/src/main/java/com/example/shaguft/twiiterclone/Home.java
@@ -2,16 +2,12 @@ package com.example.shaguft.twiiterclone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Toolbar toolbar= findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_home);
     }
 }
