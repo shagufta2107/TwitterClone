eb07f6e802f1215a20eaf8c9d71a2336aab3c30b
shagufta2107 <shagufta.zhcet09@gmail.com>
2019-01-07 23:24:07 +0530

    Added feature: passing username from login screen to home page

--- app/src/main/java/com/example/shaguft/twiiterclone/Home.java
+++ app/src/main/java/com/example/shaguft/twiiterclone/Home.java
@@ -1,7 +1,9 @@
 package com.example.shaguft.twiiterclone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("user");
+        TextView textView = (TextView)findViewById(R.id.username);
+        textView.setText(message);
     }
 }
--- app/src/main/java/com/example/shaguft/twiiterclone/MainActivity.java
+++ app/src/main/java/com/example/shaguft/twiiterclone/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +26,11 @@ public class MainActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                startActivity(new Intent(MainActivity.this, Home.class));
+                EditText usernameField = findViewById(R.id.loginUsername);
+                String username= usernameField.getText().toString();
+                Intent intent= new Intent(MainActivity.this, Home.class);
+                intent.putExtra("user",username);
+                startActivity(intent);
             }
         });
     }
